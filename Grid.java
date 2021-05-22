eda3953553da3c33958d28d447cb001e558f2469
MercuriusX <>
2021-05-22 03:35:37 +0000

    added overloaded constructor to customize board size, added pause to edit option, added try catch for invalid coordinate inputs, updated board layout, created separate edit board method

--- Grid.java
+++ Grid.java
@@ -3,56 +3,95 @@ import java.util.Scanner;
 public class Grid
 {
   private char[][] board;
+  private final char blank = 'M';
+  private final char occupied = '*';
+  private int boardSize = 25;
 
   Scanner input = new Scanner(System.in);
 
   public Grid()
   {
-    board = new char[25][25];
+    board = new char[boardSize][boardSize];
     for (int row = 0; row < board.length; row++)
     {
       for (int col = 0; col < board[0].length; col++)
       {
-        board[row][col] = 'b';
+        board[row][col] = blank;
+      }
+    }
+  }
+
+  public Grid(int s)
+  {
+    boardSize = s;
+    board = new char[s][s];
+    for (int row = 0; row < board.length; row++)
+    {
+      for (int col = 0; col < board[0].length; col++)
+      {
+        board[row][col] = blank;
       }
     }
   }
 
   public void startGame()
   {
+    editBoard();
+    String _continue = "";
+    while (_continue.equals(""))
+    {
+      board = nextGeneration();
+      System.out.println("Please press enter to continue, type pause to edit the board, or enter any other text to exit.");
+      _continue = input.nextLine();
+    }
+    if (_continue.toLowerCase().equals("pause"))
+    {
+      startGame();
+    }
+  }
+
+  public void editBoard()
+  {
     while(true)
     {
       printBoard(board);
-      System.out.println("Please enter the coordinates of your starting colony, 0-24, in the format [0][1], and press enter blank when you are done to begin.");
+      System.out.println("Please enter the coordinates of your colony, 0-" + (boardSize - 1) + ", in the format [0][1], and press enter when you are ready to start.");
       String coordinates = input.nextLine();
-      if (coordinates.toLowerCase().equals(""))
+      if (coordinates.equals(""))
       {
         break;
       }
       else
       {
-        int first = coordinates.indexOf("]");
-        int second = coordinates.lastIndexOf("]");
-        board[Integer.valueOf(coordinates.substring(1,first))][Integer.valueOf(coordinates.substring(first + 2,second))] = '*';
+        try
+        {
+          int first = coordinates.indexOf("]");
+          int second = coordinates.lastIndexOf("]");
+          board[Integer.valueOf(coordinates.substring(1,first))][Integer.valueOf(coordinates.substring(first + 2,second))] = occupied;
+        }
+        catch (Exception e)
+        {
+          System.out.println("You have entered an invalid input. Try again.");
+        }
       }
-    }
-    String _continue = "";
-    while (_continue.equals(""))
-    {
-      board = nextGeneration();
-      System.out.println("Please press enter to continue, or enter any character to exit.");
-      _continue = input.nextLine();
     }
   }
 
   public void printBoard(char[][] matrix)
   {
     System.out.println("Current Board:");
+    System.out.print(" X");
+    for (int i = 0; i < matrix.length; i++)
+    {
+      System.out.print((i >= 10) ? (i + " ") : (" " + i + " "));
+    }
+    System.out.println("");
     for (int row = 0; row < matrix.length; row++)
     {
+      System.out.print((row >= 10) ? (row + " ") : (" " + row + " "));
       for (int col = 0; col < matrix[0].length; col++)
       {
-        System.out.print(matrix[row][col]);
+        System.out.print(matrix[row][col]+ "  ");
       }
       System.out.println("");
     }
@@ -60,12 +99,12 @@ public class Grid
 
   public char[][] nextGeneration()
   {
-    char[][] nextGen = new char[25][25];
+    char[][] nextGen = new char[boardSize][boardSize];
     for (int row = 0; row < nextGen.length; row++)
     {
       for (int col = 0; col < nextGen[0].length; col++)
       {
-        nextGen[row][col] = 'b';
+        nextGen[row][col] = blank;
       }
     }
     for (int row = 0; row < nextGen.length; row++)
@@ -77,22 +116,22 @@ public class Grid
         {
           for (int j = col - 1; j <= col + 1; j++)
           {
-            if ((i != row || j != col) && (i >= 0 && i <= 24 && j >= 0 && j <= 24))
+            if ((i != row || j != col) && (i >= 0 && i <= (boardSize - 1) && j >= 0 && j <= (boardSize - 1)))
             {
-              if (board[i][j] == '*')
+              if (board[i][j] == occupied)
               {
                 counter++;
               }
             }
           }
         }
-        if ((counter < 2 || counter > 3) && board[row][col] == '*')
+        if ((counter < 2 || counter > 3) && board[row][col] == occupied)
         {
-          nextGen[row][col] = 'b';
+          nextGen[row][col] = blank;
         }
-        else if (counter == 3 && board[row][col] == 'b')
+        else if (counter == 3 && board[row][col] == blank)
         {
-          nextGen[row][col] = '*';
+          nextGen[row][col] = occupied;
         }
         else
         {
